c465d1f7221d7ce2ccd2f15543837cbed8a87077
nazirulazdie <nazirul.azdie@gmail.com>
2024-03-13 06:35:18 +0900

    add init funtion

--- script.js
+++ script.js
@@ -13,24 +13,39 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting Condition
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let activePlayer = 0;
-let currentScore = 0;
-let playing = true;
+// Starting Condition
+
+const init = function () {
+  scores = [0, 0];
+  activePlayer = 0;
+  currentScore = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
-  // console.log(`${activePlayer}`);
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
+
+// New game function
+btnNew.addEventListener('click', init);
 
 // Rolling dice function
 btnRoll.addEventListener('click', function () {
@@ -55,6 +70,7 @@ btnRoll.addEventListener('click', function () {
   }
 });
 
+// Hold score function
 btnHold.addEventListener('click', function () {
   if (playing) {
     // 1. Add current score to player score
@@ -64,7 +80,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     //2. Check if score>=100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // finish the game
       playing = false;
       diceEl.classList.add('hidden');
